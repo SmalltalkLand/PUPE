c8ff95b2a3d1106f159ed1ad80610c5c811072e8
Graham Kelly <gkgoat6700@gmail.com>
2020-09-06 08:21:57 -0400

    initial

---
+++ Windows_Installer/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aada81b4e53c74b9b36b7a25183b265e7746342e56c442c514fc086f3375315b
+size 1552
---
+++ fix-usb.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e981fa568b94fb751dea91a6c4ec8b9d2911ab99d47381c081376caa73b72155
+size 4890
